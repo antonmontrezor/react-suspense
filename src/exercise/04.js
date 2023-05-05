1b8424954efc7872af31054b5fa434c0e982341c
antonmontrezor <montrezoranton991@gmail.com>
2023-05-05 15:45:07 -0400

    finish "Cache Resource"

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -11,6 +11,8 @@ import {
 } from '../pokemon'
 import {createResource} from '../utils'
 
+const PokemonResourceCacheContext = React.createContext()
+
 function PokemonInfo({pokemonResource}) {
   const pokemon = pokemonResource.read()
   return (
@@ -29,21 +31,59 @@ const SUSPENSE_CONFIG = {
   busyMinDurationMs: 700,
 }
 
-// 🐨 create a pokemonResourceCache object
-
-// 🐨 create a getPokemonResource function which accepts a name checks the cache
-// for an existing resource. If there is none, then it creates a resource
-// and inserts it into the cache. Finally the function should return the
-// resource.
-
 function createPokemonResource(pokemonName) {
   return createResource(fetchPokemon(pokemonName))
+}
+
+function usePokemonResourceCache() {
+  return React.useContext(PokemonResourceCacheContext)
+}
+
+function PokemonCacheProvider({children, cacheTime}) {
+  const pokemonResourceCache = React.useRef({})
+  const expirations = React.useRef({})
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      for (const [name, time] of Object.entries(expirations.current)) {
+        if (time < Date.now()) {
+          delete pokemonResourceCache.current[name]
+        }
+      }
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const getPokemonResource = React.useCallback((name) => {
+    const lowerCaseName = name.toLowerCase()
+    let resource = pokemonResourceCache.current[lowerCaseName]
+    if (!resource) {
+      resource = createPokemonResource(lowerCaseName)
+      pokemonResourceCache.current[lowerCaseName] = resource
+
+      expirations.current[name] = Date.now() + cacheTime
+
+      // my approach is to setTimeout, so every time we get a resource stored in pokemonResourceCache.current on name change, we then remove it 5 seconds later
+      // and it's not dependant on component lifecycles which might make it less reliable. It's less performant as we would create multiple set timeouts instead of running an interval as a side effect
+      // setTimeout(() => {
+      //   delete pokemonResourceCache.current[lowerCaseName]
+      // }, cacheTime)
+    }
+  
+    return resource
+    // no need to use pokemonResourceCache as a dependancy, we could though since it's the same object because of userRef. If we destructure it to get current, we would anyway be using current
+    // like we do use it now since it's the same property/object within pokemonResourceCache
+  }, [cacheTime])
+
+  return <PokemonResourceCacheContext.Provider value={getPokemonResource}>{children}</PokemonResourceCacheContext.Provider>
 }
 
 function App() {
   const [pokemonName, setPokemonName] = React.useState('')
   const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG)
   const [pokemonResource, setPokemonResource] = React.useState(null)
+  const getPokemonResource = usePokemonResourceCache()
 
   React.useEffect(() => {
     if (!pokemonName) {
@@ -51,10 +91,9 @@ function App() {
       return
     }
     startTransition(() => {
-      // 🐨 change this to getPokemonResource instead
-      setPokemonResource(createPokemonResource(pokemonName))
+      setPokemonResource(getPokemonResource(pokemonName))
     })
-  }, [pokemonName, startTransition])
+  }, [getPokemonResource, pokemonName, startTransition])
 
   function handleSubmit(newPokemonName) {
     setPokemonName(newPokemonName)
@@ -88,4 +127,12 @@ function App() {
   )
 }
 
-export default App
+function AppWithProvider() {
+  return (
+    <PokemonCacheProvider cacheTime={5000}>
+      <App />
+    </PokemonCacheProvider>
+  )
+}
+
+export default AppWithProvider
